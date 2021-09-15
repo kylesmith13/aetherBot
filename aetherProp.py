713d76d6108ad2dadc886d8ff7bd7130fbaf0c2a
kyle <k.kyle.smith13@gmail.com>
2021-09-14 20:15:55 -0500

    it now produces floors and rooms

--- aetherProp.py
+++ aetherProp.py
@@ -1,31 +1,18 @@
 import json
-import sys
+import config
 from web3 import Web3
 
-infura_url = 'https://mainnet.infura.io/v3/9c9dbaa759d54bbeab399c5d671114a4'
-web3 = Web3(Web3.HTTPProvider(infura_url))
+def computeRooms(_propertyId, _progress):
+    data = __fetchDataAtProgress(_propertyId, _progress)
+    height = __computeHeightAtProgress(data['x'], data['z'], data['height'], _progress)
+    return (data['width'] * data['length']) * (round(height) - 1)
 
-# we need to make available the functionality we would like to draw from the contract. 
-# the abi could contain all functionality, but its not necessary
-abi = json.loads('[{\"constant\":true,\"inputs\":[{\"name\":\"_id\",\"type\":\"uint256\"}],\"name\":\"getProperty\",\"outputs\":[{\"name\":\"parent\",\"type\":\"uint32\"},{\"name\":\"class\",\"type\":\"uint8\"},{\"name\":\"x\",\"type\":\"uint8\"},{\"name\":\"y\",\"type\":\"uint8\"},{\"name\":\"z\",\"type\":\"uint8\"},{\"name\":\"dx\",\"type\":\"uint8\"},{\"name\":\"dz\",\"type\":\"uint8\"},{\"name\":\"height\",\"type\":\"uint8\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"}]')
-address = '0x31d4C5be1082A88F2ABAFeA549B6C189C2cf057F'
-
-contract = web3.eth.contract(address=address, abi=abi)
-
-# example response call from 103 - [3, 1, 51, 1, 36, 3, 2, 46]
-# these values are parent (district?), class, x, y, z, dx, dz, height respectively
-properties = ['parent', 'class', 'x', 'y', 'z', 'dx', 'dz', 'height']
-propertyNumber = int(sys.argv[1])
-progress = int(sys.argv[2])
-
-print(propertyNumber)
-print(progress)
-
-data = contract.functions.getProperty(propertyNumber).call()
-combined_map = dict(zip(properties, data))
+def computeHeight(_propertyId, _progress):
+    data = __fetchDataAtProgress(_propertyId, _progress)
+    return __computeHeightAtProgress(data['x'], data['z'], data['height'], _progress)
 
 # this is a private function from the contract that i've recreated to be able to use
-def computeHeightAtProgress(_x, _z, _height, progress):
+def __computeHeightAtProgress(_x, _z, _height, progress):
     x = 50 - _x if _x < 50 else _x - 50
     z = 50 - _z if _z < 50 else _z - 50
     distance = x if x > z else z
@@ -37,5 +24,22 @@ def computeHeightAtProgress(_x, _z, _height, progress):
     height = 2 * progress * _height * scale / 10000
     return height if height > 0 else 1
 
-ans = computeHeightAtProgress(combined_map['x'], combined_map['z'], combined_map['height'], progress)
-print(ans)
+def __fetchDataAtProgress(_propertyId, _progress):
+    infura_url = 'https://mainnet.infura.io/v3/' + config.infura_token
+    web3 = Web3(Web3.HTTPProvider(infura_url))
+
+    # we need to make available the functionality we would like to draw from the contract. 
+    # the abi could contain all functionality, but its not necessary
+    abi = json.loads('[{\"constant\":true,\"inputs\":[{\"name\":\"_id\",\"type\":\"uint256\"}],\"name\":\"getProperty\",\"outputs\":[{\"name\":\"parent\",\"type\":\"uint32\"},{\"name\":\"class\",\"type\":\"uint8\"},{\"name\":\"x\",\"type\":\"uint8\"},{\"name\":\"y\",\"type\":\"uint8\"},{\"name\":\"z\",\"type\":\"uint8\"},{\"name\":\"dx\",\"type\":\"uint8\"},{\"name\":\"dz\",\"type\":\"uint8\"},{\"name\":\"height\",\"type\":\"uint8\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"}]')
+    address = '0x31d4C5be1082A88F2ABAFeA549B6C189C2cf057F'
+
+    contract = web3.eth.contract(address=address, abi=abi)
+
+    # example response call from 103 - [3, 1, 51, 1, 36, 3, 2, 46]
+    # these values are parent (district?), class, x, y, z, dx, dz, height respectively
+    properties = ['parent', 'class', 'x', 'y', 'z', 'width', 'length', 'height']
+    propertyNumber = int(_propertyId)
+    progress = int(_progress)
+
+    data = contract.functions.getProperty(propertyNumber).call()
+    return dict(zip(properties, data))
--- bot.py
+++ bot.py
@@ -1,17 +1,22 @@
-import discord
 import config
+from discord.ext import commands
+import aetherProp
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as', self.user)
+bot = commands.Bot(command_prefix="$")
 
-    async def on_message(self, message):
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
+@bot.command()
+async def propertyFloorCount(ctx, propertyId, progress):
+    floors = round(aetherProp.computeHeight(int(propertyId), int(progress)))
+    response = "Property " + str(propertyId) + " will have " + str(floors) + " floors at progress " + progress + "."
+    await ctx.channel.send(response)
 
-        if message.content == 'ping':
-            await message.channel.send('pong')
+@bot.command()
+async def propertyRoomCount(ctx, propertyId, progress):
+    rooms = round(aetherProp.computeRooms(int(propertyId), int(progress)))
+    response = "Property " + str(propertyId) + " will have " + str(rooms) + " rooms at progress " + progress + "."
+    await ctx.channel.send(response)
 
-client = MyClient()
-client.run(config.token)
+bot.run(config.token)
+
+# handle messages for !propertyRoomCount and !propertyFloorCount
+# propertyRoomCount will be the dimensions of the property (2x4) * (floorCount - 1)
