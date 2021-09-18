9baa92d42493124135389f11bef9de408b4180ab
kyle <k.kyle.smith13@gmail.com>
2021-09-18 18:12:32 -0500

    still not pulling configs

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-token = os.environ['token']
-infura_token = os.environ['infura_token']
+token = os.environ['TOKEN']
+infura_token = os.environ['INFURA_TOKEN']
 contract_address = '0x31d4C5be1082A88F2ABAFeA549B6C189C2cf057F'
 contract_abi = '[{\"constant\":true,\"inputs\":[{\"name\":\"_id\",\"type\":\"uint256\"}],\"name\":\"getProperty\",\"outputs\":[{\"name\":\"parent\",\"type\":\"uint32\"},{\"name\":\"class\",\"type\":\"uint8\"},{\"name\":\"x\",\"type\":\"uint8\"},{\"name\":\"y\",\"type\":\"uint8\"},{\"name\":\"z\",\"type\":\"uint8\"},{\"name\":\"dx\",\"type\":\"uint8\"},{\"name\":\"dz\",\"type\":\"uint8\"},{\"name\":\"height\",\"type\":\"uint8\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"}]'
 
